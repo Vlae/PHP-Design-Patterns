53dc745cc981d2df02ef33e73926ecd290764e48
vlae <valeriy.tomov@gmail.com>
2018-04-17 11:34:32 +0300

    Removed singletone branch

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once 'Singletone.php';
-
-var_dump(Singletone::getInstance(['something' => 'First Value']));
-var_dump(Singletone::getInstance(['something_else' => 'Second Value']));
-
